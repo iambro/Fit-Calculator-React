d44ebf7b83aff4965258beb18a6c9276d52608e2
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-04-03 13:08:44 +0200

    Updates

---
+++ precache-manifest.567621d54a247ef230209f6ae6645ed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b31bb5ef0f2520be20a1",
+    "url": "/Fit-Calculator-React/static/css/main.161190e0.chunk.css"
+  },
+  {
+    "revision": "b31bb5ef0f2520be20a1",
+    "url": "/Fit-Calculator-React/static/js/main.f9644d96.chunk.js"
+  },
+  {
+    "revision": "075d8f1184f22e1fb36b",
+    "url": "/Fit-Calculator-React/static/js/runtime~main.2e32aebe.js"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/Fit-Calculator-React/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "3962bc70e04658f0619d72633bb47b7b",
+    "url": "/Fit-Calculator-React/index.html"
+  }
+];
